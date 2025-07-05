35b3516047e3461fa763508df237a79e25ee0e67
Debesh Sahoo <debesh.mx@gmail.com>
2025-07-05 22:51:06 +0530

    link fix
    link fix

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -79,14 +79,18 @@ const Index = () => {
             and enhanced community living experience.
           </p>
           <div className="flex flex-col sm:flex-row gap-4 justify-center">
-            <Button size="lg" className="bg-white text-emerald-700 hover:bg-emerald-50 text-lg px-8">
-              <FileText className="w-5 h-5 mr-2" />
-              Latest Notices
-            </Button>
-            <Button size="lg" variant="outline" className="bg-white text-emerald-700 hover:bg-emerald-50 text-lg px-8">
-              <MessageSquare className="w-5 h-5 mr-2" />
-              File Grievance
-            </Button>
+            <Link to="/notices">
+              <Button size="lg" className="bg-white text-emerald-700 hover:bg-emerald-50 text-lg px-8">
+                <FileText className="w-5 h-5 mr-2" />
+                Latest Notices
+              </Button>
+            </Link>
+            <Link to="/grievances">
+              <Button size="lg" variant="outline" className="bg-white text-emerald-700 hover:bg-emerald-50 text-lg px-8">
+                <MessageSquare className="w-5 h-5 mr-2" />
+                File Grievance
+              </Button>
+            </Link>
           </div>
         </div>
       </section>
